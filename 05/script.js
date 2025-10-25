486e13e6c11e6202f55568c8efa984dd9d8de8f0
Gagan <50614855+maaynussih@users.noreply.github.com>
2025-10-25 17:39:04 +0530

    DataTypes

---
+++ 05/script.js
@@ -0,0 +1,47 @@
+// Datatypes
+// Numbers
+let num1 = 3.14;
+let num2 = 10;
+// implicit typecasting 
+console.log(num1 + num2);
+
+
+// Strings
+const firstName = "Ayush";
+const lastName= "Kush"
+console.log( firstName + lastName);
+
+
+
+// Booleans
+let isLoggedIN = true;
+console.log(isLoggedIN);
+
+let lastLoggedIN = null;
+
+// undefined
+let lastLoggedin = undefined;
+
+// object creation
+const person = {
+    firstName : "Ayush",
+    lastName : "Kush",
+    age : 25,
+    isLoggedIN : false,
+    lastLoggedIN : null
+}
+console.log(person);
+
+
+console.log(isLoggedIN+10)
+// False=0 True=1
+// one string  one number when multiplied add to become number 
+
+ console.log('1'*1);   
+   
+//  type of opertor 
+
+console.log(typeof(firstName));
+console.log(typeof(person));
+
+
