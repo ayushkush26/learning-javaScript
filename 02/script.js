70849402e1fa438ae23cf6af12aec7a98fcbd1c8
Gagan <50614855+maaynussih@users.noreply.github.com>
2025-10-25 16:39:59 +0530

    variables

---
+++ 02/script.js
@@ -0,0 +1,38 @@
+// Variables
+// Var 
+
+// DataTypes varName = value ; Storngly typed ...
+// JS is loos Typed lang ..
+
+// var age = 22;    // we can re-assign the Var  
+
+// Naming  Conv.
+// firstname 
+// first_name
+// FirstName 
+// firstname  (Pref) Cammel Case
+
+// Var -> scope is Global..
+
+if (true) {
+    var age = 23;
+}
+console.log(age)
+
+// let -> Scopr of the let is local ..
+
+if (true) {
+    let a = 24;
+}
+console.log(a);
+
+
+
+// Const  -> scope of the Const is local               
+// we can't declare const without initializing values. 
+const b = 23;     // We can't change the value of the const but we can modifiy it by performing operations ...
+
+
+
+
+
