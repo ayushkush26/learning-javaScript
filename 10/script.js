c2356c3c2a587a13155541dbed849438435229c2
Gagan <50614855+maaynussih@users.noreply.github.com>
2025-10-25 23:03:52 +0530

    Arrays Basics

---
+++ 10/script.js
@@ -0,0 +1,25 @@
+// Arrays 
+
+const students =['Ayush','Kush','Pranav','Apoorv'];
+console.log(students);
+
+console.log(students.length);
+console.log(students[0]);
+students[1] = 'Gagan';
+console.log(students);
+
+students.push("Tom")
+console.log(students);
+
+const myArray =[1,3.24,"Ayush"];
+// Hetrogenous 
+
+console.log(myArray)
+
+console.log(myArray.indexOf("Ayush"))
+
+students.pop();
+console.log(students)
+
+students.reverse()
+console.log(students)
